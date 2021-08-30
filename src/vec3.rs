cc677ec540d4a45e11e3086cdd6524b49bf84a14
KentaKudo <yd37574@gmail.com>
2021-08-30 11:54:20 +0100

    The Vec3 Class

---
+++ src/vec3.rs
@@ -0,0 +1,154 @@
+#[derive(Copy, Clone)]
+pub struct Vec3(f64, f64, f64);
+
+type Point3 = Vec3;
+pub type Color = Vec3;
+
+impl Vec3 {
+    fn default() -> Self {
+        Self(0.0, 0.0, 0.0)
+    }
+
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self(x, y, z)
+    }
+
+    fn x(self) -> f64 {
+        self.0
+    }
+
+    fn y(self) -> f64 {
+        self.1
+    }
+
+    fn z(self) -> f64 {
+        self.2
+    }
+
+    fn length(self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn length_squared(self) -> f64 {
+        self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
+    }
+}
+
+use std::ops;
+
+impl ops::Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0, -self.1, -self.2)
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(
+            self.x() + other.x(),
+            self.y() + other.y(),
+            self.z() + other.z(),
+        )
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z())
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self(self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z())
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3(self * rhs.x(), self * rhs.y(), self * rhs.z())
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        rhs * self
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self {
+        (1.0 / rhs) * self
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self(
+            self.x() + other.x(),
+            self.y() + other.y(),
+            self.z() + other.z(),
+        );
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self(self.x() * other, self.y() * other, self.z() * other);
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        *self *= 1.0 / other;
+    }
+}
+
+impl Vec3 {
+    fn dot(self, other: Self) -> f64 {
+        self.x() * other.x() + self.y() + other.y() + self.z() + other.z()
+    }
+
+    fn cross(self, other: Self) -> Self {
+        Self(
+            self.y() * other.z() - self.z() * other.y(),
+            self.z() * other.x() - self.x() * other.z(),
+            self.x() * other.y() - self.y() * other.x(),
+        )
+    }
+
+    fn unit_vector(self) -> Self {
+        self / self.length()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.x(), self.y(), self.z())
+    }
+}
+
+pub fn print_color(pixel_color: Color) {
+    println!(
+        "{} {} {}",
+        (255.999 * pixel_color.x()) as i32,
+        (255.999 * pixel_color.y()) as i32,
+        (255.999 * pixel_color.z()) as i32,
+    )
+}
